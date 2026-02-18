33f53f020d65a06b1ac2b81e82b3c04c77e6c09e
NathanTheAsian <91707385+NathanTheAsian@users.noreply.github.com>
2026-02-17 21:49:26 -0500

    Update Post.java

--- labs/04/Post.java
+++ labs/04/Post.java
@@ -8,33 +8,42 @@ public class Post implements Likeable, Comparable<Post> {
     private String userName;
 
     public Post(String userName) {
-      // Your code here
+        this.userName = userName;
+        this.likes = 0;
+        this.timeStamp = Calendar.getInstance().getTime();
     }
 
     public String getUserName() {
-	     return userName;
+        return userName;
     }
 
     public Date getTimeStamp() {
-	     return timeStamp;
+        return timeStamp;
     }
 
-    // Implement the methods required by the interface Likeable.
-    // This file will not compile unless they are present with the correct name and signature.
+    public int getLikes() {
+        return likes;
+    }
 
+    // Likeable interface methods
+    @Override
+    public void like() {
+        likes++;
+    }
+
+    @Override
     public String toString() {
-    	String str = new String();
-    	str = getClass().getName() + ": " + timeStamp + ", " + userName + ", likes = " + likes;
-    	return  str;
+        String str = new String();
+        str = getClass().getName() + ": " + timeStamp + ", " + userName + ", likes = " + likes;
+        return str;
     }
 
+    @Override
+    public int compareTo(Post other) {
+        return this.timeStamp.compareTo(other.timeStamp);
+    }
 
-  	public int compareTo(Post other){
-  		// Your code here.
-  	}
-
-  	public boolean isPopular(){
-  		// Your code here.
-  	}
-
+    public boolean isPopular() {
+        return likes > 100;
+    }
 }
